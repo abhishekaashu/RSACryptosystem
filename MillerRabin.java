ad8f2cb50d9d9a2fa005bee839eef29a19d3810f
Sahil Mishra <sahilmishra0012@outlook.com>
2019-01-02 09:42:04 +0530

    Miller Rabin test update

--- MillerRabin.java
+++ MillerRabin.java
@@ -1,62 +1,54 @@
 import java.util.*;
   
-class MillerRabin { 
-
-    static int power(int x, int y, int p) { 
-          
+class MillerRabin 
+{ 
+    static int power(int x, int y, int p) 
+    {           
         int res = 1;
-
         x = x % p;  
-  
         while (y > 0) { 
             if ((y & 1) == 1) 
                 res = (res * x) % p; 
-
             y = y >> 1; // y = y/2 
             x = (x * x) % p; 
         } 
-          
         return res; 
-    } 
-
-    static boolean miillerTest(int d, int n) { 
+    }
+    static boolean miillerTest(int d, int n)
+    { 
         int a = 2 + (int)(Math.random() % (n - 4)); 
-
         int x = power(a, d, n); 
-      
         if (x == 1 || x == n - 1) 
             return true; 
         while (d != n - 1) { 
             x = (x * x) % n; 
             d *= 2; 
-          
             if (x == 1) 
                 return false; 
             if (x == n - 1) 
                 return true; 
         } 
-
         return false; 
     } 
-    static boolean isPrime(int n, int k) { 
+
+    static boolean isPrime(int n, int k)
+    { 
 
         if (n <= 1 || n == 4) 
             return false; 
         if (n <= 3) 
             return true; 
-        int d = n - 1; 
-          
+        int d = n - 1;       
         while (d % 2 == 0) 
             d /= 2; 
         for (int i = 0; i < k; i++) 
             if (!miillerTest(d, n)) 
                 return false; 
-      
         return true; 
     } 
 
-    public static void main(String args[]) { 
-          
+    public static void main(String args[])
+    {       
 		int k = 4;
 		int n;
 		Scanner sc=new Scanner(System.in);
