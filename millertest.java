dbf2655102483d402ea71b0817000d88d22b3ccf
Sahil Mishra <sahilmishra0012@outlook.com>
2019-01-31 12:24:19 +0530

    New Changes

--- millertest.java
+++ millertest.java
@@ -8,14 +8,16 @@ class test1
         BigInteger a = new BigInteger(n.intValue(), rand);
         a=a.add(BigInteger.TWO);
         BigInteger x = modexp(a, d, n); 
-        if (x.compareTo(BigInteger.ONE)==0 || x == n.subtract(BigInteger.ONE))
+        if (x.equals(BigInteger.ONE) || x.equals(n.subtract(BigInteger.ONE)))
             return true; 
-        while (d != n.subtract(BigInteger.ONE)) { 
+        while (!(d.equals(n.subtract(BigInteger.ONE))))
+        {
+            System.out.println("Hi");
             x = (x.multiply(x)).mod(n); 
             d=d.multiply(BigInteger.TWO); 
-            if (x.compareTo(BigInteger.ONE)==0) 
+            if (x.equals(BigInteger.ONE)) 
                 return false; 
-            if (x ==n.subtract(BigInteger.ONE)) 
+            if (x.equals(n.subtract(BigInteger.ONE)))
                 return true; 
         } 
         return false; 
@@ -23,12 +25,13 @@ class test1
     static boolean isPrime(BigInteger n, int k)
     { 
 
-        if (n.compareTo(BigInteger.ONE)<=0 || n.compareTo(BigInteger.valueOf(4))==0) 
+        if (n.compareTo(BigInteger.ONE)<=0 || n.equals(BigInteger.valueOf(4)))
             return false; 
-        if (n.compareTo(BigInteger.valueOf(3))<0) 
+        if (n.compareTo(BigInteger.valueOf(3))<=0) 
             return true; 
         BigInteger d= n.subtract(BigInteger.ONE);
-        while (d.mod(BigInteger.valueOf(2)).compareTo(BigInteger.TWO)==0) 
+        while (d.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) 
+            System.out.println("This");
             d=d.divide(BigInteger.TWO); 
         for (int i = 0; i < k; i++) 
             if (!millerTest(d, n)) 
@@ -37,11 +40,11 @@ class test1
     }
     static BigInteger modexp(BigInteger x,BigInteger y,BigInteger p)
     {
-        BigInteger res=BigInteger.valueOf(1);
+        BigInteger res=BigInteger.ONE;
         x=x.mod(p);
         while(y.compareTo(BigInteger.ZERO)==1)
         {
-            if((y.mod(BigInteger.TWO)).compareTo(BigInteger.ONE)==0)//y is odd
+            if(y.and(BigInteger.ONE).equals(BigInteger.ONE))//y is odd
             {
                 res=(res.multiply(x)).mod(p);
             }
@@ -55,45 +58,8 @@ class test1
         Scanner sc=new Scanner(System.in);
         String s1=sc.nextLine();
         BigInteger a=new BigInteger(s1);
-        boolean d=isPrime(a,4);
+        boolean d=isPrime(a,10);
         System.out.println(d);
         sc.close();
     }
 }
-
-
-
-    /*static boolean millerTest(BigInteger d, BigInteger n)
-    {
-        Random rand = new Random();
-        BigInteger a = new BigInteger(n.intValue(), rand);
-        a=a.add(BigInteger.TWO);
-        BigInteger x = modexp(a, d, n); 
-        if (x.compareTo(BigInteger.ONE)==0 || x == n.subtract(BigInteger.ONE))
-            return true; 
-        while (d != n.subtract(BigInteger.ONE)) { 
-            x = (x.multiply(x)).mod(n); 
-            d=d.multiply(BigInteger.TWO); 
-            if (x.compareTo(BigInteger.ONE)==0) 
-                return false; 
-            if (x ==n.subtract(BigInteger.ONE)) 
-                return true; 
-        } 
-        return false; 
-    }
-
-    static boolean isPrime(BigInteger n, int k)
-    { 
-
-        if (n.compareTo(BigInteger.ONE)<=0 || n.compareTo(BigInteger.valueOf(4))==0) 
-            return false; 
-        if (n.compareTo(BigInteger.valueOf(3))<0) 
-            return true; 
-        BigInteger d= n.subtract(BigInteger.ONE);
-        while (d.mod(BigInteger.valueOf(2)).compareTo(BigInteger.TWO)==0) 
-            d=d.divide(BigInteger.TWO); 
-        for (int i = 0; i < k; i++) 
-            if (!millerTest(d, n)) 
-                return false; 
-        return true; 
-    }*/
