c23d3a87c61c1b21119aa4243e7b4dcfe1a1134e
Sahil Mishra <sahilmishra0012@outlook.com>
2019-03-05 12:14:38 +0530

    new changes

--- RSA/millertest.java
+++ RSA/millertest.java
@@ -18,6 +18,7 @@ class MillerTest
         } 
         return false; 
     }
+
     static boolean isPrime(BigInteger n, int k)
     { 
 
@@ -35,6 +36,7 @@ class MillerTest
                 return false; 
         return true; 
     }
+    
     static BigInteger modexp(BigInteger x,BigInteger y,BigInteger p)
     {
         BigInteger res=BigInteger.ONE;
